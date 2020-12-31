6048b83059aeee8eb3181a538a37a911241d4412
Dr Mich Talebzadeh <mich@peridale.co.uk>
2020-12-31 09:55:24 +0000

    major changes to analyse_UKHousing.py

--- src/analyze_UKHousing.py
+++ src/analyze_UKHousing.py
@@ -4,7 +4,6 @@ from pyspark.sql import functions as F
 from pyspark.sql.functions import col, round
 from pyspark.sql.window import Window
 from pyspark.sql.functions import lag
-import conf.variables as v
 from sparkutils import sparkstuff as s
 import usedFunctions as uf
 from pyhive import hive
@@ -20,6 +19,11 @@ from pandas.plotting import scatter_matrix
 import six
 import locale
 locale.setlocale(locale.LC_ALL, 'en_GB')
+try:
+  import variables as v
+except ModuleNotFoundError:
+  from conf import variables as v
+
 appName = "ukhouseprices"
 spark = s.spark_session(appName)
 spark.sparkContext._conf.setAll(v.settings)
@@ -36,7 +40,7 @@ print("\nStarted at");uf.println(lst)
 if (spark.sql(f"""SHOW TABLES IN {v.DSDB} like '{tableName}'""").count() == 1):
     spark.sql(f"""ANALYZE TABLE {fullyQualifiedTableName} compute statistics""")
     rows = spark.sql(f"""SELECT COUNT(1) FROM {fullyQualifiedTableName}""").collect()[0][0]
-    print("number of rows is ",rows)
+    print("Total number of rows is ",rows)
 else:
     print(f"""No such table {fullyQualifiedTableName}""")
     sys.exit(1)
@@ -44,11 +48,12 @@ f"""
 https://stackoverflow.com/questions/59278835/pyspark-how-to-write-dataframe-partition-by-year-month-day-hour-sub-directory
 """
 wSpecY = Window().partitionBy(F.date_format('datetaken',"yyyy"))
-wSpecM = Window().partitionBy(F.date_format('datetaken',"yyyy"), F.date_format('datetaken',"MM"))  ## partion by Year and Month
-
-print(f"""\nAnnual House prices in {regionname} in GBP""")
 
 house_df = spark.sql(f"""select * from {fullyQualifiedTableName} where regionname = '{regionname}'""")
+rows = spark.sql(f"""SELECT COUNT(1) FROM {fullyQualifiedTableName} where regionname = '{regionname}'""").collect()[0][0]
+print(f"Total number of rows for {regionname} is ", rows)
+
+print(f"""\nAnnual House prices in {regionname} in GBP""")
 
 df2 = house_df.filter(col("datetaken").between(f'{start_date}', f'{end_date}')). \
                 select( \
@@ -63,9 +68,9 @@ df2 = house_df.filter(col("datetaken").between(f'{start_date}', f'{end_date}')).
 df2.show(10,False)
 df2.write.mode("overwrite").saveAsTable(f"""{v.DSDB}.yearlyhouseprices""")
 
-wSpecP = Window().orderBy('year')
+wSpecPY = Window().orderBy('year')
 
-df_lag = df2.withColumn("prev_year_value", F.lag(df2['AVGPricePerYear']).over(wSpecP))
+df_lag = df2.withColumn("prev_year_value", F.lag(df2['AVGPricePerYear']).over(wSpecPY))
 result = df_lag.withColumn('percent_change', F.when(F.isnull(df2.AVGPricePerYear - df_lag.prev_year_value),0). \
                            otherwise(F.round(((df2.AVGPricePerYear-df_lag.prev_year_value)*100.)/df_lag.prev_year_value,1)))
 
@@ -73,9 +78,9 @@ print(f"""\nPercent annual Average House prices change in {regionname}""")
 
 rs = result.select('Year', 'AVGPricePerYear', 'prev_year_value', 'percent_change')
 rs.show()
-rs.write.mode("overwrite").saveAsTable(f"""{v.DSDB}.percenthousepricechange""")
+rs.write.mode("overwrite").saveAsTable(f"""{v.DSDB}.percentyearlyhousepricechange""")
 
-print(f"""\nMonthly House prices in {regionname} in GBP""")
+wSpecM = Window().partitionBy(F.date_format('datetaken',"yyyy"), F.date_format('datetaken',"MM"))  ## partion by Year and Month
 
 df3 = house_df.filter(col("datetaken").between('2018-01-01', '2020-01-01')). \
                 select( \
@@ -86,7 +91,16 @@ df3 = house_df.filter(col("datetaken").between('2018-01-01', '2020-01-01')). \
                     , round(F.avg('SemiDetachedPrice').over(wSpecM)).alias('AVGSemiDetachedPricePerMonth') \
                     , round(F.avg('DetachedPrice').over(wSpecM)).alias('AVGDetachedPricePerMonth')). \
                 distinct().orderBy('datetaken', asending=True)
-df3.show(120,False)
-df3.write.mode("overwrite").saveAsTable(f"""{v.DSDB}.monthlyhouseprices""")
+
+wSpecPM = Window().orderBy('Year-Month')
+
+df_lagM = df3.withColumn("prev_month_value", F.lag(df3['AVGPricePerMonth']).over(wSpecPM))
+resultM = df_lagM.withColumn('percent_change', F.when(F.isnull(df3.AVGPricePerMonth - df_lagM.prev_month_value),0). \
+                         otherwise(F.round(((df3.AVGPricePerMonth-df_lagM.prev_month_value)*100.)/df_lagM.prev_month_value,1)))
+print(f"""\nMonthly House price changes in {regionname} in GBP""")
+
+rsM = resultM.select('Year-Month', 'AVGPricePerMonth', 'prev_month_value', 'percent_change')
+rsM.show(36,False)
+rsM.write.mode("overwrite").saveAsTable(f"""{v.DSDB}.percentmonthlythousepricechange""")
 lst = (spark.sql("SELECT FROM_unixtime(unix_timestamp(), 'dd/MM/yyyy HH:mm:ss.ss') ")).collect()
 print("\nFinished at");uf.println(lst)
